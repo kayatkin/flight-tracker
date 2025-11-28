f8b0ba5bcfb9431fbc601e43c378e44f93ddfa32
kayatkin <v.kayatkin@gmail.com>
2025-11-28 10:16:28 +0700

    Supabase setup

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://izwtfezysnzweeftxkhk.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6d3RmZXp5c256d2VlZnR4a2hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzQyNTMsImV4cCI6MjA3OTc1MDI1M30.dRw-f9BSZ2p2jldeClT7Bc1G4l5-gfZQmNFSGt-Ieus';
+const supabaseUrl = 'https://iptnzxnbcrxczcowjead.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwdG56eG5iY3J4Y3pjb3dqZWFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyODk0NDgsImV4cCI6MjA3OTg2NTQ0OH0.rKvAmtkFKCMY3j6pshDscm6uxk0CH0RQnIsCPaEZ_I0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
